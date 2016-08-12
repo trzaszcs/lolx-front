c11de7472e802bb7b115b356cfc2952fbf6221f1
Adi Perek <the.adrian.perek@gmail.com>
2016-08-12 08:41:38 +0000

    update

--- src/components/AnounceCard.vue
+++ src/components/AnounceCard.vue
@@ -1,27 +1,28 @@
 <template>
    <div class="ui container">
-     <div class="segment">
-       <div class="card">
-        <div class="content">
-          <img class="right floated mini ui image" src="../assets/plumber.png">
-          <div class="header">
-            {{anounce.title}}
+     <div class="ui segment">
+         <div class="card">
+          <div class="content">
+            <img class="right floated mini ui image" src="../assets/plumber.png">
+            <div class="header">
+              {{anounce.title}}
+            </div>
+            <div class="meta">
+              Jan Kowalski - {{anounce.city}} - {{anounce.state}}
+            </div>
+            <div class="description">
+              {{anounce.desc}}
+            </div>
           </div>
-          <div class="meta">
-            Jan Kowalski - {{anounce.city}} - {{anounce.state}}
-          </div>
-          <div class="description">
-            {{anounce.desc}}
+          <div class="extra content">
+            <div class="ui two buttons">
+              <div class="ui basic green button">Zamów</div>
+              <div class="ui basic red button">Anuluj</div>
+            </div>
           </div>
         </div>
-        <div class="extra content">
-          <div class="ui two buttons">
-            <div class="ui basic green button">Zamów</div>
-            <div class="ui basic red button">Anuluj</div>
-          </div>
-        </div>
-      </div>
     </div>
+    
   </div>
 </template>
 
