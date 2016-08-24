f89ab398ef1bd1021191421a32c9a0a7f5d3bd7f
Adi Perek <the.adrian.perek@gmail.com>
2016-08-24 19:11:23 +0000

    time piker

--- src/components/AnounceCard.vue
+++ src/components/AnounceCard.vue
@@ -24,8 +24,34 @@
         
         <div class="extra content">
           <div class="ui form">
-            <div class="ten wide field">
-              <label>Termin wykonania usługi</label>
+            
+            <div class="field">
+              <label>Preferowana godzina wykonania usługi</label>
+              <select class="ui search dropdown">
+                <option value="">godzina</option>
+                <option value="06:00">06:00</option>
+                <option value="07:00">07:00</option>
+                <option value="08:00">08:00</option>
+                <option value="09:00">09:00</option>
+                <option value="10:00">10:00</option>
+                <option value="11:00">11:00</option>
+                <option value="12:00">12:00</option>
+                <option value="13:00">13:00</option>
+                <option value="14:00">14:00</option>
+                <option value="15:00">15:00</option>
+                <option value="16:00">16:00</option>
+                <option value="17:00">17:00</option>
+                <option value="18:00">18:00</option>
+                <option value="19:00">19:00</option>
+                <option value="20:00">20:00</option>
+                <option value="21:00">21:00</option>
+                <option value="22:00">22:00</option>
+                <option value="23:00">23:00</option>
+              </select>
+            </div>
+            
+            <div class="field">
+              <label>oraz data</label>
                 <div class="ui input">
                   <input type="text" id="daterange"/>
                  </div>
@@ -119,7 +145,10 @@ export default {
     }
   },
   ready: function () {
-    $('#daterange').daterangepicker()
+    $('#daterange').daterangepicker({
+      singleDatePicker: true,
+      showDropdowns: true
+    })
   }
 }
 </script>
