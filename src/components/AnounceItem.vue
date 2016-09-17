2758d7ff901a713a394d6352b072b7ba40746376
Slawomir T <s.trzaszczka@wp.pl>
2016-09-18 00:36:27 +0200

    date format on listing

--- src/components/AnounceItem.vue
+++ src/components/AnounceItem.vue
@@ -12,7 +12,7 @@
       </div>
       <div class="extra">
         {{anounce.city}} ({{anounce.state}}) <br/>
-        {{anounce.creationDate}}
+        {{creationDate()}}
       </div>
    </div>
  </div>
@@ -23,6 +23,26 @@ export default {
   props: ['anounce'],
   data () {
     return {
+      creationDate: function () {
+        const anounceDate = new Date(this.anounce.creationDate)
+        const diff = new Date() - anounceDate
+        const oneHour = 60 * 60 * 1000
+        const hours = diff / oneHour
+        const days = hours / 24
+        const timeStr = () => {
+          const twoDigits = (value) => { return ('0' + value).slice(-2) }
+          const hours = twoDigits(anounceDate.getHours())
+          const minutes = twoDigits(anounceDate.getMinutes())
+          return `${hours}:${minutes}`
+        }
+        if (hours < 24) {
+          return `dziś ${timeStr()}`
+        } else if (hours < 48) {
+          return `wczoraj ${timeStr()}`
+        } else {
+          return `${days} dni temu`
+        }
+      }
     }
   },
   methods: {
