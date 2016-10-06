847eaa4c6ed41018749beb4d1a92f3b67f70de5e
Slawomir Trzaszczka <slawomir.trzaszczka@allegrogroup.com>
2016-10-06 17:12:21 +0200

    category added

---
+++ src/components/CategoryBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model="categoryId" class="ui dropdown">
+    <option value="">Kategoria...</option>
+    <option  v-for="category in categories" v-bind:value="category.id">
+      {{category.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+import $ from 'jquery'
+import api from '../api'
+
+export default {
+  data () {
+    return {
+      categories: []
+    }
+  },
+  methods: {
+    selected: function (categoryId) {
+      console.log('selected category' + categoryId)
+    }
+  },
+  ready: function () {
+    api.categories((categories) => {
+      this.categories = categories
+    })
+    $('.ui.dropdown').dropdown()
+  }
+}
+</script>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="ui three column stackable grid">
-    <div class="eight wide column">
+    <div class="six wide column">
       <div class="ui left icon fluid input">
         <input v-model="phrase" type="text" placeholder="Poszukuje kogoś kto..." v-on:keyup.13="search"/>
         <i class="search icon"></i>
       </div>
     </div>
-    <div class="six wide column">
+    <div class="four wide column">
       <location-input :location="location" :simple="true"></location-input>
+    </div>
+    <div class="three wide column">
+      <category-box></category-box>
     </div>
     <div class="one wide column">
       <button v-on:click="search" class="ui teal button">Szukaj</button>
@@ -17,9 +20,10 @@
 
 <script>
 import LocationInput from './LocationInput'
+import CategoryBox from './CategoryBox'
 
 export default {
-  components: {LocationInput},
+  components: {LocationInput, CategoryBox},
   data () {
     return {
       phrase: '',
