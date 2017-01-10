65f1c7605b7e6404a8012f330224804683683473
trzaszcs <s.trzaszczka@wp.pl>
2017-01-10 22:29:04 +0100

    my orders impr

--- src/components/myRequestOrders/RequestOrder.vue
+++ src/components/myRequestOrders/RequestOrder.vue
@@ -1,34 +1,18 @@
 <template>
 
-  <div>
-    <a target="_blank">{{requestOrder.anounceTitle}}</a> <br/>
+  <div v-on:click="details()">
+    <a>{{requestOrder.anounceTitle}}</a> <br/>
     <span class="author">{{requestOrder.authorNamePretty}}</span> zamówił <span class="creationDate">{{requestOrder.creationDatePretty}}</span> <br/>
     Status: <span class="status">{{requestOrder.statusPretty}}</span>
-    <br/>
-    <a v-if="requestOrder.deleteAllowed" v-on:click="deleteRequestOrder()">Usuń</a>
-    <a v-if="requestOrder.rejectAllowed" v-on:click="reject()">Odrzuć</a>
-    <a v-if="requestOrder.acceptAllowed" v-on:click="accept()">Akceptuj</a>
   </div>
-            
+
 </template>
 <script>
 export default {
   props: ['requestOrder'],
   methods: {
-    deleteRequestOrder: function () {
-      if (window.confirm('Czy na pewno chcesz usunąć zamówienie?')) {
-        this.$dispatch('delete', {id: this.requestOrder.id})
-      }
-    },
-    reject: function () {
-      if (window.confirm('Czy na pewno chcesz odrzucić zamówienie?')) {
-        this.$dispatch('reject', {id: this.requestOrder.id})
-      }
-    },
-    accept: function () {
-      if (window.confirm('Czy na pewno chcesz potwierdzić zamówienie?')) {
-        this.$dispatch('accept', {id: this.requestOrder.id})
-      }
+    details: function () {
+      this.$router.go({path: '/order', query: {orderId: this.requestOrder.id}})
     }
   }
 }
