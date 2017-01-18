71875b90ef3af9bced23ac3974784d716bf9fcc9
trzaszcs <s.trzaszczka@wp.pl>
2017-01-18 21:23:10 +0100

    ro listing improved

--- src/components/myRequestOrders/RequestOrder.vue
+++ src/components/myRequestOrders/RequestOrder.vue
@@ -2,7 +2,7 @@
 
   <div v-on:click="details()">
     <a>{{requestOrder.anounceTitle}}</a> <br/>
-    <span class="author">{{requestOrder.authorNamePretty}}</span> zamówił <span class="creationDate">{{requestOrder.creationDatePretty}}</span> <br/>
+    Zamówienie od <span class="author">{{requestOrder.authorName}}</span> <span class="creationDate">{{requestOrder.creationDatePretty}}</span> <br/>
     Status: <span class="status">{{requestOrder.statusPretty}}</span>
   </div>
 
