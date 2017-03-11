177e5982f4502706df2d07873da7115ae2352007
Adi Perek <the.adrian.perek@gmail.com>
2017-03-11 22:03:22 +0000

    add users stars to comments

--- src/components/UserComments.vue
+++ src/components/UserComments.vue
@@ -3,7 +3,8 @@
  <div class="ui fluid selection list" v-for="comment in comments">
     <div class="ui item">
       <div class="left floated content">
-        <div class="ui mini star rating" data-max-rating="5" data-rating="comment.stars" id="rating"></div>
+        <div class="ui mini star rating" data-max-rating="5" data-rating="comment.stars" id="comment.nick"></div>
+        ({{comment.stars}}/5)
       </div>
       <div class="content">
           <a class="header">
