2700c8c4ed9005d7a3257e38130a1a99b30a0b52
Adi Perek <the.adrian.perek@gmail.com>
2017-03-27 17:58:29 +0000

    date added

--- src/components/UserComments.vue
+++ src/components/UserComments.vue
@@ -2,15 +2,15 @@
           
  <div class="ui fluid selection list" v-for="comment in comments">
     <div class="ui item">
-      <div class="left floated content">
-        {{comment.date}}
-      </div>
       <div class="right floated content">
         <b>{{comment.nick}}</b>
         <i class="ui yellow star icon"></i>
         {{comment.stars}}/5 
       </div>
       <div class="meta">
+        <div class="left floated content">
+          {{comment.date}}
+        </div>
         <p>
           {{comment.msg}}
         </p>
