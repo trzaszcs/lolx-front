32dd1123ce6017d50ad6812f01d2296e3cbd90d7
trzaszcs <s.trzaszczka@wp.pl>
2017-04-16 15:58:41 +0200

    hamburder menu added

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,8 @@
           <div class="ui secondary inverted pointing menu">
             <a class="ui tiny image" v-link="{ path: '/' }"><img src="./assets/logo-big.png"></a>
             <div class="right item">
-              <button class="ui icon black button" v-link="{ path: '/search' }" data-tooltip="Nowe wyszukanie" data-position="bottom center">
-                  <img  src="./assets/search.png"class="ui mini image"></img> 
-              </button>
-              <button class="ui icon black button" v-link="{ path: '/addAnounce' }" data-tooltip="Dodaj ogłoszenie" data-position="bottom center">
-                  <img  src="./assets/add.png"class="ui mini image"></img> 
-              </button>
-              <button class="ui icon black button" v-link="{ path: '/myAccount' }" data-tooltip="Moje Konto" data-position="bottom center">
-                  <img  src="./assets/profile.png"class="ui mini image"></img> 
-              </button>
-
+              <toolbar-menu></toolbar-menu>
               <toolbar-events v-if="logged"></toolbar-events>
-
             </div>
           </div>
         </div>
@@ -39,12 +29,14 @@
 <script>
 import PageFooter from './components/PageFooter.vue'
 import ToolbarEvents from './components/ToolbarEvents.vue'
+import ToolbarMenu from './components/ToolbarMenu.vue'
 import session from './session'
 
 export default {
   components: {
     PageFooter,
-    ToolbarEvents
+    ToolbarEvents,
+    ToolbarMenu
   },
   data () {
     return {
---
+++ src/components/ToolbarMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <button id="toolbarMenu" class="ui icon black button">
+    <img src="../assets/menu.png" class="ui mini image"></img>
+  </button>
+  
+  <div class="ui fluid popup bottom center">
+    <ul>
+      <li>
+        <a v-link="{ path: '/search' }" data-tooltip="Nowe wyszukanie">
+           Znajdź ogłoszenie
+        </a>
+      </li>
+      <li>
+        <a v-link="{ path: '/addAnounce' }" data-tooltip="Dodaj ogłoszenie">
+           Dodaj ogłoszenie
+        </a>
+      </li>
+      <li>
+        <a v-link="{ path: '/myAccount' }" data-tooltip="Moje Konto">
+           Moje konto
+        </a>
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  data () {
+    return {
+      eventsMap: {},
+      noOfEvents: 0
+    }
+  },
+  methods: {
+    showUpdatedChats: function () {
+      this.$router.go({path: '/myAccount', query: {section: 'chats'}})
+    },
+    showRequestOrders: function () {
+      this.$router.go({path: '/myAccount', query: {section: 'requestOrders'}})
+    }
+  },
+  ready: function () {
+    setTimeout(() => {
+      $('#toolbarMenu').popup({
+        popup: $('.popup'),
+        on: 'click'
+      }) }, 0)
+  }
+}
+</script>
