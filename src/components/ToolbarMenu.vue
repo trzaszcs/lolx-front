7618c966fd7c15ae15523f7e7e5e534338b88dd0
trzaszcs <s.trzaszczka@wp.pl>
2017-04-18 23:46:07 +0200

    toolbarMenu hamburder

--- src/components/ToolbarMenu.vue
+++ src/components/ToolbarMenu.vue
@@ -1,26 +1,29 @@
 <template>
-  <button id="toolbarMenu" class="ui icon black button">
-    <img src="../assets/menu.png" class="ui mini image"></img>
-  </button>
+  <div class="toolbarMenu">
+    <button class="ui icon black button">
+      <img src="../assets/menu.png" class="ui mini image"></img>
+    </button>
   
-  <div class="ui fluid popup bottom center">
-    <ul>
-      <li>
-        <a v-link="{ path: '/search' }" data-tooltip="Nowe wyszukanie">
-           Znajdź ogłoszenie
-        </a>
-      </li>
-      <li>
-        <a v-link="{ path: '/addAnounce' }" data-tooltip="Dodaj ogłoszenie">
-           Dodaj ogłoszenie
-        </a>
-      </li>
-      <li>
-        <a v-link="{ path: '/myAccount' }" data-tooltip="Moje Konto">
-           Moje konto
-        </a>
-      </li>
-    </ul>
+    <div class="ui popup left" style="width: 180px">
+      <ul>
+        <li>
+          <a v-link="{ path: '/search' }" data-tooltip="Nowe wyszukanie">
+             Znajdź ogłoszenie
+          </a>
+        </li>
+        <li>
+          <a v-link="{ path: '/addAnounce' }" data-tooltip="Dodaj ogłoszenie">
+            Dodaj ogłoszenie
+          </a>
+        </li>
+        <li>
+          <a v-link="{ path: '/myAccount' }" data-tooltip="Moje Konto">
+            Moje konto
+          </a>
+        </li>
+      </ul>
+    </div>
+
   </div>
   
 </template>
@@ -45,10 +48,22 @@ export default {
   },
   ready: function () {
     setTimeout(() => {
-      $('#toolbarMenu').popup({
-        popup: $('.popup'),
-        on: 'click'
-      }) }, 0)
+      let $toolbarMenu = $('.toolbarMenu')
+      let $popup = $toolbarMenu.find('.popup')
+      let $button = $toolbarMenu.find('button')
+      const topPosition = $button.position().top
+      const leftPosition = $button.position().left - $popup.width() - 40
+      $popup.css('left', leftPosition)
+      $popup.css('top', topPosition)
+      $button.click(() => {
+        $popup.toggle()
+      })
+      $(document).mouseup((e) => {
+        if (e.which === 1 && !$button.is(e.target) && $button.has(e.target).length === 0) {
+          $popup.hide()
+        }
+      })
+    }, 0)
   }
 }
 </script>
