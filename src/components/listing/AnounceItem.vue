c786131c18d1e89b1295e476840141d69fa77c4f
trzaszcs <s.trzaszczka@wp.pl>
2017-06-06 20:56:05 +0200

    listing styles improved

--- src/components/listing/AnounceItem.vue
+++ src/components/listing/AnounceItem.vue
@@ -1,50 +1,26 @@
 <template>
   <div class="item">
-                 
-    <div class="ui stackable grid">  
-      <div class="row">
-        <div class="column">
-          <h3 class="ui header">
-            <a v-link="{ path: '/anounce', query: { anounceId: anounce.id }}">
+    <img class="ui small image" :src="anounce.img" style="float:left">
+    <div class="content">
+      <h3 class="title">
+          <a v-link="{ path: '/anounce', query: { anounceId: anounce.id }}">
               {{anounce.title}}
-            </a>
-          </h3>
-        </div>
-      </div>
-      
-      <div class="four wide column">
-          <img class="ui small image" :src="anounce.img">
-      </div>
-
-      <div class="ten wide column">
-        <h4 class="ui header">{{anounce.location.title}}</h4>
-        <div class="content">
-          <span class="category">{{anounce.categoryName}}</span>
-          {{creationDate()}}
-        </div>
-      </div>
-
-      <div class="two wide left aligned column">
-        <h4 class="ui header">Cena</h4>
-        <div class="content">
+          </a>
+      </h3>
+      <span class="category">{{anounce.categoryName}}</span>
+      <div class="price">
           {{anounce.price}} zł
-        </div>
       </div>
+      <div class="location">{{anounce.location.title}}</div>
     </div>
- 
-   </div>
+  </div>
 </template>
 
 <script>
-import util from '../../util'
-
 export default {
   props: ['anounce'],
   data () {
     return {
-      creationDate: function () {
-        return util.prettyDate(this.anounce.creationDate)
-      }
     }
   }
 }
